a85e0904b4bee081d0ad9e92d1f09af9b51b283b
dsarok <ananddivyashankar@gmail.com>
2021-06-17 23:36:19 +0530

    all of the for methods

--- merge.js
+++ merge.js
@@ -1,64 +1,19 @@
-let arr=[12,19,-3,29,0,-5,65,34,120];
-function merge(st,mid,end){
-	
-	let arr1=[];
-	for(let i=st;i<=mid;i++){
-		arr1.push(arr[i]);
-	}
-	let arr2=[];
-	for(let j=mid+1;j<=end;j++){
-		arr2.push(arr[j]);
-	}
-	let p1=0;p2=0;
-	arr3=[];
-	while(p1<arr1.length&&p2<arr2.length){
-		if (arr1[p1]<arr2[p2]) {
-			arr3.push(arr1[p1]);
-			p1=p1+1;
-		}
-		else{
-			arr3.push(arr2[p2]);
-			p2=p2+1;
-		}
-	}
-	while(p1<arr1.length){
-		arr3.push(arr1[p1]);
-		p1=p1+1;
-	}
-	while(p2<arr2.length){
-		arr3.push(arr2[p2]);
-		p2=p2+1;
-	}
-	for(let i=st;i<=end;i++){
-		arr[i]=arr3[i-st];
-	}
-	return ;
+let arr=[2 ,3,6,2,6,64,75];
+for (let index = 0; index < arr.length; index++) {
+	const element = arr[index];
+	console.log(element);
 }
-
-function mergesort(st,end) {
-	
-	if (end-st===0) {
-		return;
+let obj={
+	member1:'divya shankar ',
+	member2:'sonu kumar',
+	member3:'hero number 1',
+	fingit(){
+		console.log("jio mere bete");
 	}
-	else if (end-st===1) {
-		if (arr[st]>arr[end]) {
-			let x=arr[st];
-			arr[st]=arr[end];
-			arr[end]=x;
-			return;
-		}
-		else{
-			return;
-		}
-	}
-	let l=st;let h=end;
-	let mid=Math.floor((l+h)/2);
-
-	mergesort(st,mid);
-	mergesort(mid+1,end);
-	merge(st,mid,end);
 }
-let atom=0.5;
-console.log(arr);
-mergesort(0,arr.length-1);
-console.log(arr);
+for (const e in obj) {
+	console.log(e);
+}
+for (const x of arr) {
+	console.log(x);
+}
